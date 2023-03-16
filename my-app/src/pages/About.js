ffca6120179f47d444072a9dd76e450da7c4b75c
Yeyzon <yeyzon75@gmail.com>
2023-03-16 08:26:35 -0600

    Se agregó el Task

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -5,7 +5,19 @@ function About() {
     return (
         <div className="about-container">
             <h1>About</h1>
-            <p>This is a simple To-Do List app created with React.</p>
+            <p>A To-Do List is a tool that helps you manage your tasks, keep track of your progress, and stay organized. It can be used for personal or professional purposes, and is an effective way to improve productivity and manage time more efficiently.</p>
+            <h2>Key Features</h2>
+            <ul>
+                <li>Add, edit, and delete tasks</li>
+                <li>Set deadlines and reminders</li>
+                <li>Assign priorities and categories</li>
+                <li>Filter and sort tasks</li>
+                <li>Collaborate with others</li>
+            </ul>
+            <h2>About Us</h2>
+            <p>We are a team of passionate developers dedicated to creating innovative solutions that improve people's lives. Our mission is to simplify the way people work, learn, and connect with each other.</p>
+            <h2>Contact Us</h2>
+            <p>If you have any questions or issues with our app, please don't hesitate to contact us at support@mytodolist.com.</p>
         </div>
     );
 }
--- my-app/src/pages/Task.js
+++ my-app/src/pages/Task.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState } from "react";
+import "../styles/Task.css";
 
-function Tasks() {
+function Task() {
+    // Definir el estado para la lista de tareas y la tarea actual
+    const [taskList, setTaskList] = useState([]);
+    const [currentTask, setCurrentTask] = useState("");
+
+    // Función para agregar una tarea a la lista
+    const handleAddTask = () => {
+        if (currentTask !== "") {
+            setTaskList([...taskList, currentTask]);
+            setCurrentTask("");
+        }
+    };
+
+    // Función para eliminar una tarea de la lista
+    const handleDeleteTask = (taskIndex) => {
+        const newList = [...taskList];
+        newList.splice(taskIndex, 1);
+        setTaskList(newList);
+    };
+
+    // Función para marcar una tarea como completada
+    const handleCompleteTask = (taskIndex) => {
+        const newList = [...taskList];
+        newList[taskIndex] = "✓ " + newList[taskIndex];
+        setTaskList(newList);
+    };
+
+    // Función para editar una tarea de la lista
+    const handleEditTask = (taskIndex) => {
+        setCurrentTask(taskList[taskIndex]);
+        handleDeleteTask(taskIndex);
+    };
+
     return (
-        <div className="page task-page">
+        <div className="task-container">
+            <h1>Task List</h1>
+            <div className="task-input-container">
+                <input
+                    type="text"
+                    placeholder="Add a new task"
+                    value={currentTask}
+                    onChange={(e) => setCurrentTask(e.target.value)}
+                />
+                <button onClick={handleAddTask}>Add Task</button>
+            </div>
+            <div className="task-list-container">
+                <ul className="task-list">
+                    {taskList.map((task, index) => (
+                        <li key={index}>
+                            {task}
+                            <div className="task-buttons">
+                                <button onClick={() => handleCompleteTask(index)}>✓</button>
+                                <button onClick={() => handleEditTask(index)}>✎</button>
+                                <button onClick={() => handleDeleteTask(index)}>✕</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
 
-export default Tasks;
+export default Task;
+
