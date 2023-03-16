851383e0a7d7c82d651d2da4aef4b080b49fc9eb
Yeyzon <yeyzon75@gmail.com>
2023-03-16 12:34:52 -0600

    Arreglando el NavBar.css

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,24 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import "../styles/NavBar.css";
 
 function NavBar() {
+    const [active, setActive] = useState(false);
+
+    function handleClick() {
+        setActive(!active);
+    }
+
     return (
-        <nav className="navbar">
-            <Link to="/pages/Home" className="navbar-brand">
-                ToDoList
-            </Link>
-            <div className="navbar-links">
-                <Link to="/pages/Home" className="navbar-link">
-                    Home
-                </Link>
-                <Link to="/pages/Task" className="navbar-link">
-                    Tasks
-                </Link>
-                <Link to="/pages/About" className="navbar-link">
-                    About
-                </Link>
+        <nav>
+            <div className="navbar-logo">
+                <a href="../pages/Home">Logo</a>
             </div>
+            <ul className="navbar-links">
+                <li><a href="../pages/Home">Home</a></li>
+                <li><a href="../pages/Task">Task</a></li>
+                <li><a href="../pages/About">About</a></li>
+            </ul>
+            <div className="navbar-hamburger" onClick={handleClick}>
+                <div className={`hamburger ${active ? "active" : ""}`}></div>
+            </div>
+            <ul className={`navbar-links-mobile ${active ? "active" : ""}`}>
+                <li><a href="../pages/Home">Home</a></li>
+                <li><a href="../pages/Task">Task</a></li>
+                <li><a href="../pages/About">About</a></li>
+            </ul>
         </nav>
     );
 }
